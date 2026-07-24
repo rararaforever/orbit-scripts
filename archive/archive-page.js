eac9c1645c2053623ce81dca12b64202ea01c4e1
ɹ̰ᴉ̰ɯⱯ̰ <95018580+PascalWager@users.noreply.github.com>
2026-07-24 14:13:31 -0400

    ss

--- archive/archive-page.js
+++ archive/archive-page.js
@@ -1,5 +1,6 @@
-const params = new URLSearchParams(window.location.search);
-const slug = params.get("slug");
+// const params = new URLSearchParams(window.location.search);
+// const slug = params.get("slug");
+const slug = window.location.hash.substring(1);
 
 console.log(slug); // "apple"
 if (data) console.log(data);
